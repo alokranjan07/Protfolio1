b529044864a0d6dcab6dd6f8d899a2ec625de633
alokranjan07 <145465021+alokranjan07@users.noreply.github.com>
2024-08-13 18:12:25 +0000

    protfolio

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <div>
       <Router>
         <Navbar/>
+         
 
         <Routes>
 
--- protfolio/src/Component/Navbar/Navbar.jsx
+++ protfolio/src/Component/Navbar/Navbar.jsx
@@ -3,14 +3,23 @@ import  { Link }  from 'react-router-dom'
 import './Navbar.css'
 function Navbar() {
   return (
-    <div>
-     
-      <Link to='home'> Home</Link>
-      <Link to='protfolio'>Protfolio</Link>
-      <Link to='about'>About</Link>
-      <Link to='education'>Education</Link>
+    <div className="nav">
+     <ul className="ul">
+      <li>
+      <Link to='home' className="link"> Home</Link>
+      </li>
+      <li>
+
+      <Link to='protfolio' className="link">Protfolio</Link>
+      </li>
+      <li>
+      <Link to='about' className="link">About</Link>
+      </li>
+      <li>
+      <Link to='education' className="link">Education</Link>
+      </li>
       
-      
+      </ul>
 
     </div>
   )
